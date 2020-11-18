1b9d6c3365d68fe5533bc9f1e0bca1f6f6f1633b
InB4DevOps <73686890+InB4DevOps@users.noreply.github.com>
2020-11-18 12:31:32 +0100

    Inital commit for vqm gui

--- utils.py
+++ utils.py
@@ -1,11 +1,22 @@
 import sys
+import math
 from time import time
 
 from ffmpeg import probe
 
 
+def get_pretty_codec_name(codec):
+    dict = {
+                'h264': 'H.264 (AVC)',
+                'hevc': 'H.265 (HEVC)'
+            }
+
+    return dict.get(codec, codec)
+
+
 def line():
-    print('-----------------------------------------------------------------------------------------------------------')
+    print('-------------------------------------------------------------------\
+          ----------------------------------------')
 
 
 def subprocess_printer(message, arguments_list):
@@ -19,7 +30,7 @@ def is_list(argument_object):
 
 
 def force_decimal_places(value, decimal_places):
-	return '{:0.{dp}f}'.format(value, dp=decimal_places)
+    return '{:0.{dp}f}'.format(value, dp=decimal_places)
 
 
 def exit_program(message):
@@ -33,12 +44,24 @@ class VideoInfoProvider:
     def __init__(self, video_path):
         self._video_path = video_path
 
-    def get_bitrate(self, video_path=None):
+    def get_bitrate(self, video_path=None, unit='Kbps'):
         if video_path:
             bitrate = probe(video_path)['format']['bit_rate'] 
         else:
             bitrate = probe(self._video_path)['format']['bit_rate']
-        return f'{round(int(bitrate) / 1_000_000, 3)} Mbps'
+        if unit == 'Kbps':
+            return f'{math.trunc(int(bitrate) / 1000)} Kbps'
+        elif unit == 'Mbps':
+            return f'{round(int(bitrate) / 1000000, 2)} Mbps'
+
+    def get_duration(self):
+        return probe(self._video_path)['format']['duration']
+
+    def get_encoder(self):
+        encoder = [stream for stream in probe(self._video_path)['streams']
+                   if stream['codec_type'] == 'video'][0]['codec_name']
+
+        return get_pretty_codec_name(encoder)
 
     def get_framerate_fraction(self):
         r_frame_rate = [stream for stream in probe(self._video_path)['streams']
@@ -49,8 +72,36 @@ class VideoInfoProvider:
         numerator, denominator = self.get_framerate_fraction().split('/')
         return round((int(numerator) / int(denominator)), 3)
 
-    def get_duration(self):
-        return probe(self._video_path)['format']['duration']
+    def get_pretty_duration(self):
+        duration = round(float(self.get_duration()))
+        return time.strftime('%H:%M:%S', time.gmtime(duration))
+
+    def get_resolution(self):
+        width = [stream for stream in probe(self._video_path)['streams']
+                 if stream['codec_type'] == 'video'][0]['width']
+        height = [stream for stream in probe(self._video_path)['streams']
+                  if stream['codec_type'] == 'video'][0]['height']
+
+        return f'{width}x{height}'
+
+    def get_statusbar_text(self):
+        bitrate = self.get_bitrate(unit='Mbps')
+        return f'Video File: {Path(self._video_path).name} | ' + \
+               f'{self.get_encoder()} | ' + \
+               f'{self.get_resolution()} | ' + \
+               f'{self.get_pretty_duration()} | ' + \
+               f'{bitrate}'
+
+    def is_file_a_video_file(self):
+        try:
+            _ = self.get_duration()
+            return True
+        except Exception:
+            return False
+
+    @property
+    def file_name(self):
+        return Path(self._video_path).name
 
 
 class Timer:
@@ -59,5 +110,7 @@ class Timer:
 
     def end(self, decimal_places):
         time_to_convert = time() - self.__start_time
-        time_rounded = force_decimal_places(round(time_to_convert, decimal_places), decimal_places)
+        time_rounded = force_decimal_places(round(time_to_convert,
+                                                  decimal_places),
+                                            decimal_places)
         return time_rounded
